540d8136048c6ba6523418ede08694c330c539c1
Nico Herlim <nicoherlim2003@gmail.com>
2024-12-08 15:43:09 +0700

    wow

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel from "laravel-vite-plugin";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: path.join(__dirname, "/resources/images"),
+                    dest: path.join(__dirname, "/public"),
+                },
+                {
+                    src: path.join(__dirname, "/resources/favicon"),
+                    dest: path.join(__dirname, "/public"),
+                },
+                {
+                    src: path.join(__dirname, "/resources/favicon/favicon.ico"),
+                    dest: path.join(__dirname, "/public"),
+                },
+            ],
+        }),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
